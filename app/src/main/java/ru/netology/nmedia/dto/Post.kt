49ec45c4eefeebbd9f0b4c65f635680b949a8e79
anastas <avmurueva@gmail.com>
2020-10-25 23:27:04 +0300

    2.2 MVVM

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -5,7 +5,7 @@ data class Post(
     val author: String,
     val content: String,
     val published: String,
-    var likebyMe: Boolean = false,
-    var likes: Int = 0,
-    var share: Int = 0
+    val likedByMe: Boolean = false,
+    val likes: Int = 0,
+    val share: Int = 0
 )
