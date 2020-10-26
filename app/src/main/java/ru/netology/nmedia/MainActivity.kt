edf52a10fe082fe46b8833878961889178dc7b2e
anastas <avmurueva@gmail.com>
2020-10-26 15:50:17 +0300

    2.3 RecyclerView + like

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,11 +3,8 @@ package ru.netology.nmedia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -18,42 +15,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                txtAuthor.text = post.author
-                txtPublished.text = post.published
-                txtContent.text = post.content
-                txtLikes.text = numbersString(post.likes)
-                txtShare.text = numbersString(post.share)
-
-                imgLike.setImageResource(
-                    if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_favorite_24
-                )
-            }
+        val adapter = PostsAdapter{
+            viewModel.likeById(it.id)
         }
-        binding.imgLike.setOnClickListener {
-            viewModel.like()
-        }
-        binding.imgShare.setOnClickListener {
-            viewModel.share()
-        }
-    }
-
-    fun numbersString(number: Int): String {
-        return when (number) {
-            in 0..999 -> number.toString()
-            in 1_000..9_999 -> if ((number / 100) % 10 == 0) {
-                (number / 1_000).toString() + "K"
-            } else {
-                String.format("%.1f", number * 1.0 / 1_000) + "K"
-            }
-            in 10_000..999_999 -> (number / 1_000).toString() + "K"
-            else -> if ((number / 100_000) % 10 == 0) {
-                (number / 1000_000).toString() + "M"
-            } else {
-                String.format("%.1f", number * 1.0 / 1000_000) + "M"
-            }
-
+        binding.listView.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.list = posts
         }
     }
 }
+/*
+val viewModel: PostViewModel by viewModels()
+viewModel.data.observe(this) { post ->
+    with(binding) {
+        txtAuthor.text = post.author
+        txtPublished.text = post.published
+        txtContent.text = post.content
+        txtLikes.text = numbersString(post.likes)
+        txtShare.text = numbersString(post.share)
+
+        imgLike.setImageResource(
+            if (post.likedByMe) ru.netology.nmedia.R.drawable.ic_liked_24 else ru.netology.nmedia.R.drawable.ic_favorite_24
+        )
+    }
+}
+binding.imgLike.setOnClickListener {
+    viewModel.like()
+}
+binding.imgShare.setOnClickListener {
+    viewModel.share()
+}*/
---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -0,0 +1,74 @@
+package ru.netology.nmedia.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.CardPostBinding
+import ru.netology.nmedia.dto.Post
+
+typealias OnLikeListener = (post: Post) -> Unit
+
+class PostsAdapter(private val onLikeListener: OnLikeListener): RecyclerView.Adapter<PostViewHolder>() {
+    var list = emptyList<Post>()
+    set(value) {
+        field = value
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PostViewHolder(binding, onLikeListener)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val post = list[position]
+        holder.bind(post)
+    }
+
+    override fun getItemCount(): Int = list.size
+
+
+}
+
+class PostViewHolder(
+    private val binding: CardPostBinding,
+    private val onLikeListener: OnLikeListener
+): RecyclerView.ViewHolder(binding.root) {
+    fun bind(post: Post) {
+        binding.apply {
+            txtAuthor.text = post.author
+            txtPublished.text = post.published
+            txtContent.text = post.content
+            txtLikes.text = numbersString(post.likes)
+            txtShare.text = numbersString(post.share)
+
+            imgLike.setImageResource(
+                if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_favorite_24
+            )
+            imgLike.setOnClickListener {
+                onLikeListener(post)
+            }
+
+
+        }
+    }
+
+    fun numbersString(number: Int): String {
+        return when (number) {
+            in 0..999 -> number.toString()
+            in 1_000..9_999 -> if ((number / 100) % 10 == 0) {
+                (number / 1_000).toString() + "K"
+            } else {
+                String.format("%.1f", number * 1.0 / 1_000) + "K"
+            }
+            in 10_000..999_999 -> (number / 1_000).toString() + "K"
+            else -> if ((number / 100_000) % 10 == 0) {
+                (number / 1000_000).toString() + "M"
+            } else {
+                String.format("%.1f", number * 1.0 / 1000_000) + "M"
+            }
+
+        }
+    }
+}
